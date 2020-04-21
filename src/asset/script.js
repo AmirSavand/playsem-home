e7dda4054723b05acf56d274d7e51e1cb27fb296
Amir Savand <amir@savandbros.com>
2020-04-21 18:07:20 +0300

    Initial v2.0 commit

--- src/asset/script.js
+++ src/asset/script.js
@@ -4,3 +4,16 @@
 if (document.cookie.match(/^(.*;)?\s*token\s*=\s*[^;]+(.*)?$/)) {
   location.href = 'https://app.playsem.com/';
 }
+
+/**
+ * Set section height to screen size
+ */
+for (const event of ['orientationchange', 'load', 'resize']) {
+  window.addEventListener(event, () => {
+    document.querySelectorAll('#main').forEach(element => {
+      const height = window.innerHeight - document.getElementsByTagName('header')[0].clientHeight;
+      element.style.minHeight = height + 'px';
+    });
+  });
+}
+
