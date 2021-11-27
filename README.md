99b3dfbf09c3355d9cc2bfb4dc1846ae3769b42f
Amir Savand <amir@savandbros.com>
2021-11-27 17:01:23 +0300

    Use FTP for deployment (instead of GitHub pages)
    [skip ci]

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# [PlaysEM Home](https://www.playsem.com/)
+# [PlaysEM Home](https://playsem.com)
+
+[![Deployment](https://github.com/AmirSavand/playsem-home/actions/workflows/deployment.yml/badge.svg?branch=production)](https://github.com/AmirSavand/playsem-home/actions/workflows/deployment.yml)
 
 Host and join parties and keep shuffling sick songs.
--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,19 @@
 
 const csso = require('gulp-csso');
 const del = require('del');
-const file = require('gulp-file');
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify-es').default;
 const connect = require('gulp-connect');
-const ghpages = require('gulp-gh-pages');
+const replace = require('gulp-replace');
+
+const currentYear = new Date().getFullYear();
 
 // CLEAN
 
 gulp.task('clean', () => {
-  return del(['dist', 'serve', 'deploy']);
+  return del(['dist', 'serve']);
 });
 
 // BUILD
@@ -21,12 +22,7 @@ gulp.task('clean', () => {
 gulp.task('build:styles', () => {
   return gulp.src('src/asset/**/*.scss')
     // Compile SASS files
-    .pipe(sass({
-      outputStyle: 'nested',
-      precision: 10,
-      includePaths: ['.'],
-      onError: console.error.bind(console, 'Sass error:'),
-    }))
+    .pipe(sass().on("error", sass.logError))
     // Minify the file
     .pipe(csso())
     // Output
@@ -43,6 +39,7 @@ gulp.task('build:scripts', () => {
 
 gulp.task('build:html', () => {
   return gulp.src(['src/**/*.html'])
+    .pipe(replace("CURRENT_YEAR", String(currentYear)))
     .pipe(htmlmin({
       collapseWhitespace: true,
       removeComments: true,
@@ -60,17 +57,14 @@ gulp.task('build', gulp.series('clean', 'build:styles', 'build:scripts', 'build:
 
 gulp.task('serve:styles', () => {
   return gulp.src('src/asset/style.scss')
-    .pipe(sass({
-      outputStyle: 'nested',
-      precision: 10,
-      includePaths: ['.'],
-      onError: console.error.bind(console, 'Sass error:'),
-    }))
+    .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest('serve/asset/'));
 });
 
 gulp.task('serve:copy', () => {
-  return gulp.src(['src/**/*', '!src/**/*.scss']).pipe(gulp.dest('serve'));
+  return gulp.src(['src/**/*', '!src/**/*.scss'])
+    .pipe(replace("CURRENT_YEAR", String(currentYear)))
+    .pipe(gulp.dest('serve'));
 });
 
 gulp.task('serve:reload', () => {
@@ -90,20 +84,6 @@ gulp.task('serve:connect', async () => {
 });
 
 gulp.task('serve', gulp.series('clean', 'serve:copy', 'serve:styles', 'serve:reload', 'serve:connect', 'serve:watch'));
-
-// DEPLOY
-
-gulp.task('deploy:clean', gulp.series('build', () => {
-  return gulp.src('dist/**/*').pipe(ghpages());
-}));
-
-gulp.task('deploy', gulp.series('build', () => {
-  return gulp.src('dist/**/*')
-    .pipe(file('CNAME', 'www.playsem.com'))
-    .pipe(ghpages({
-      cacheDir: 'deploy',
-    }));
-}));
 
 // DEFAULT
 
